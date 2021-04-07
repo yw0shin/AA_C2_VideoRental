cd48d7072914e5815b9064c42681497fa8c9bde6
katz <katz@LAPTOP-6G9P3QBE>
2021-04-07 13:56:43 +0900

    1. Video Class SubClassing 실시 및 Factory 생성

--- Rental.java
+++ Rental.java
@@ -56,13 +56,9 @@ public class Rental {
 			long diff = new Date().getTime() - rentDate.getTime();
 			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
 		}
-		if ( daysRented <= 2) return limit ;
 
-		switch ( video.getVideoType() ) {
-			case Video.VHS: limit = 5 ; break ;
-			case Video.CD: limit = 3 ; break ;
-			case Video.DVD: limit = 2 ; break ;
-		}
-		return limit ;
+		//polymorphism 통한 case, 코드 중복 제거 
+		if ( daysRented <= 2) return 0 ;
+		else return video.getRentLimit();
 	}
 }
