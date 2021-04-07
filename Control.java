cb55b7faf9d13940654efc9e64fb9e8267bf7a3e
shalrs <shalrs@DESKTOP-C4M8E6F>
2021-04-07 14:49:45 +0900

    Use video factory for init VR

--- Control.java
+++ Control.java
@@ -87,8 +87,8 @@ public class Control {
 		customers.add(james) ;
 		customers.add(brown) ;
 
-		Video v1 = new Video("v1", Video.CD, Video.REGULAR, new Date()) ;
-		Video v2 = new Video("v2", Video.DVD, Video.NEW_RELEASE, new Date()) ;
+		Video v1 = VideoFactory.getInstance("v1", VideoFactory.CD, Video.REGULAR, new Date());
+		Video v2 = VideoFactory.getInstance("v2", VideoFactory.DVD, Video.NEW_RELEASE, new Date());
 		videos.add(v1) ;
 		videos.add(v2) ;
 
